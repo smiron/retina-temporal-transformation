1bdb5f231fd7eab26622f81cff3d6be309f06986
maricel.caciula@gmail.com <maricel.caciula@gmail.com@b1336879-68ee-e147-e23c-903a86bb8c45>
2013-07-09 14:45:21 +0000

    Started to add graphs

---
+++ TemporalEncoding/TemporalEncoding/RealtimeGraphItem.cs
@@ -0,0 +1,10 @@
+﻿using Righthand.RealtimeGraph;
+
+namespace TemporalEncoding
+{
+    public class RealtimeGraphItem : IGraphItem
+    {
+        public int Time { get; set; }
+        public double Value { get; set; }
+    }
+}
